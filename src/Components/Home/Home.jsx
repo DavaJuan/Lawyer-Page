979084b8d54f0860c17a00708b466fa6e5d98d98
DavaJuan <davalosjuanpablo038@gmail.com>
2023-03-13 11:02:16 -0300

    add button

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className='container-home'>
         <h1>Jose Felix - Abogado</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> Cupiditate sequi dignissimos,<br/> repudiandae vel nulla. <br/>Neque obcaecati tempore labore hic voluptates.</p>
-        <button>Contactame</button>
+        <button data-aos="fade-right">Contactame</button>
       </div>
     </div>
   );
